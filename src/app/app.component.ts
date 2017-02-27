e60f327f292e5503e603706ade89484be9f61d73
Semenihin Maksim <semenihin.maksim@gmail.com>
2017-02-27 19:59:38 +0200

    dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
-import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
-
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: 'my-app',
+  template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent {
-  title = 'app works!';
-}
+export class AppComponent  { name = 'Angular'; }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,11 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
+import { AppComponent }  from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MyNewComponentComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
